30e0f15fa1adece98452b5b6ec5417f571d9d234
Kadir Yuecel <ky@clicktrend.de>
2014-09-02 07:51:33 +0200

    CalculatorFactory added

--- dist/moonalizer.js
+++ dist/moonalizer.js
@@ -394,19 +394,57 @@ PLEASE DO NOT REMOVE THIS COPYRIGHT BLOCK.
 */ 
 
 
+var CalculatorFactory = {
+  create: function (date, criterion) {
+    return new Calculator(date, CriterionFactory.create(criterion));
+  }
+};;//--------------------- Copyright Block ----------------------
+/*
+ Moonalizer.js
+ 
+ Copyright (C) 2014 moonalizer.com
+ 
+ Developer: Kadir Yücel
+ Company: Click Trend Media GmbH clicktrend-media.com
+ License: GNU LGPL v3.0
+ 
+ TERMS OF USE:
+ Permission is granted to use this code, with or
+ without modification, in any website or application
+ provided that credit is given to the original work
+ with a link back to moonalizer.com.
+ 
+ This program is distributed in the hope that it will
+ be useful, but WITHOUT ANY WARRANTY.
+ 
+ PLEASE DO NOT REMOVE THIS COPYRIGHT BLOCK.
+ 
+ */
+
+var createDiyanet = function (algorithm) {
+  if (algorithm instanceof Algorithm) {
+    return new Diyanet(new algorithm());
+  } else {
+    return new Diyanet(new SunCalcWrap());
+  }
+};
+
+var createYallop = function (algorithm) {
+  if (algorithm instanceof Algorithm) {
+    return new Yallop(new algorithm());
+  } else {
+    return new Yallop(new SunCalcWrap());
+  }
+};
+
 var CriterionFactory = {
-  createDiyanet: function (algorithm) {
-    if(algorithm instanceof Algorithm) {
-      return new Diyanet(new algorithm());
+  create: function (criterion, algorithm) {
+    if (criterion === 'diyanet') {
+      return createDiyanet(algorithm);
+    } else if (criterion === 'yallop') {
+      return createYallop(algorithm);
     } else {
-      return new Diyanet(new SunCalcWrap());
-    }
-  },
-  createYallop: function (algorithm) {
-    if(algorithm instanceof Algorithm) {
-      return new Yallop(new algorithm());
-    } else {
-      return new Yallop(new SunCalcWrap());
+      return createYallop(algorithm);
     }
   }
 };;//--------------------- Copyright Block ----------------------
--- src/factory/criterion.js
+++ src/factory/criterion.js
@@ -1,40 +1,50 @@
 //--------------------- Copyright Block ----------------------
 /*
-Moonalizer.js
+ Moonalizer.js
+ 
+ Copyright (C) 2014 moonalizer.com
+ 
+ Developer: Kadir Yücel
+ Company: Click Trend Media GmbH clicktrend-media.com
+ License: GNU LGPL v3.0
+ 
+ TERMS OF USE:
+ Permission is granted to use this code, with or
+ without modification, in any website or application
+ provided that credit is given to the original work
+ with a link back to moonalizer.com.
+ 
+ This program is distributed in the hope that it will
+ be useful, but WITHOUT ANY WARRANTY.
+ 
+ PLEASE DO NOT REMOVE THIS COPYRIGHT BLOCK.
+ 
+ */
 
-Copyright (C) 2014 moonalizer.com
+var createDiyanet = function (algorithm) {
+  if (algorithm instanceof Algorithm) {
+    return new Diyanet(new algorithm());
+  } else {
+    return new Diyanet(new SunCalcWrap());
+  }
+};
 
-Developer: Kadir Yücel
-Company: Click Trend Media GmbH clicktrend-media.com
-License: GNU LGPL v3.0
-
-TERMS OF USE:
-Permission is granted to use this code, with or
-without modification, in any website or application
-provided that credit is given to the original work
-with a link back to moonalizer.com.
-
-This program is distributed in the hope that it will
-be useful, but WITHOUT ANY WARRANTY.
-
-PLEASE DO NOT REMOVE THIS COPYRIGHT BLOCK.
-
-*/ 
-
+var createYallop = function (algorithm) {
+  if (algorithm instanceof Algorithm) {
+    return new Yallop(new algorithm());
+  } else {
+    return new Yallop(new SunCalcWrap());
+  }
+};
 
 var CriterionFactory = {
-  createDiyanet: function (algorithm) {
-    if(algorithm instanceof Algorithm) {
-      return new Diyanet(new algorithm());
+  create: function (criterion, algorithm) {
+    if (criterion === 'diyanet') {
+      return createDiyanet(algorithm);
+    } else if (criterion === 'yallop') {
+      return createYallop(algorithm);
     } else {
-      return new Diyanet(new SunCalcWrap());
-    }
-  },
-  createYallop: function (algorithm) {
-    if(algorithm instanceof Algorithm) {
-      return new Yallop(new algorithm());
-    } else {
-      return new Yallop(new SunCalcWrap());
+      return createYallop(algorithm);
     }
   }
 };
